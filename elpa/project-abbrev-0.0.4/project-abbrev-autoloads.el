1243de17f7d65483d0d91f93acd102d6b4d3a8c2
GLZ17 <1213173752@qq.com>
2021-07-07 20:42:57 +0800

    add project abbr

---
+++ elpa/project-abbrev-0.0.4/project-abbrev-autoloads.el
@@ -0,0 +1,26 @@
+;;; project-abbrev-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "project-abbrev" "project-abbrev.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from project-abbrev.el
+
+(autoload 'project-abbrev-complete-word "project-abbrev" "\
+Complete the current word that point currently on." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "project-abbrev" '("project-abbrev-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; project-abbrev-autoloads.el ends here
--- init.el
+++ init.el
@@ -123,17 +123,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(auto-complete-clang-async company zenburn-theme counsel project-abbrev htmlize hungry-delete php-mode typescript-mode haskell-mode smartparens popwin)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  ;; '(hl-line ((t (:background "#586F3E" :foreground "#dddddd"))))
- ;; '(region ((t (:background "#863434" :foreground "#cccccc"))))
  '(hl-line ((t (:background "#2e2e2e"))))
- '(region ((t (:background "#2C55A7"))))
- )
+ '(region ((t (:background "#2C55A7")))))
 
 
 
